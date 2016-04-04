39cc8aa6a393fd81db16941236a19fc8d0518d32
bartlettmic <bartlettmic@gmail.com>
2016-04-04 17:00:38 -0600

    Detective notes in File menu

--- src/clueGame/ClueGame.java
+++ src/clueGame/ClueGame.java
@@ -2,8 +2,10 @@ package clueGame;
 
 import java.awt.BorderLayout;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class ClueGame extends JFrame {
 	public static Board board;
@@ -16,7 +18,51 @@ public class ClueGame extends JFrame {
 		board = new Board();
 		board.initialize();
 		add(board, BorderLayout.CENTER);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		menuBar.add(createFileMenu());		
+
 	}
+
+	private JMenu createFileMenu()
+	{
+		JMenu menu = new JMenu("File"); 
+		menu.add(createDetectiveNotes());
+		menu.add(createFileExitItem());		
+		return menu;
+	}
+
+	private JMenuItem createFileExitItem()
+	{
+		JMenuItem item = new JMenuItem("Exit");
+		class MenuItemListener implements ActionListener {
+			public void actionPerformed(ActionEvent e)
+			{
+				System.exit(0);
+			}
+		}
+		item.addActionListener(new MenuItemListener());
+
+		return item;
+	}
+	
+	private JMenuItem createDetectiveNotes()
+	{
+		JMenuItem item = new JMenuItem("Detective Notes");
+		class MenuItemListener implements ActionListener {
+			public void actionPerformed(ActionEvent e)
+			{
+				DetectiveNotes notes = new DetectiveNotes();
+				notes.setSize(500,500);
+				notes.setVisible(true);
+			}
+		}
+		item.addActionListener(new MenuItemListener());
+
+		return item;
+	}
+	
 
 	public static void main(String[] args) {
 		ClueGame ourGame = new ClueGame();
@@ -24,14 +70,9 @@ public class ClueGame extends JFrame {
 		
 		ourGame.setSize((board.getNumRows()*40>1000) ? board.getNumRows()*40 : 1000, board.getNumColumns()*40);
 		
-		DetectiveNotes notes = new DetectiveNotes();
-		notes.setSize(500,500);
-		notes.setVisible(true);
-		
-		
 		ControlGUI gui = new ControlGUI();
 		ourGame.add(gui, BorderLayout.SOUTH);
-		ourGame.setVisible(true);
+		ourGame.setVisible(true);		
 		
 	}
 }
