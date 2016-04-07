29a2f3606c8833e0bc7107a426575c5651cd7248
unknown <jmenard@BB316B-15.adit.mines.edu>
2016-04-07 16:44:59 -0600

    Skeleton code for displaying cards

---
+++ src/clueGame/HumanCards.java
@@ -0,0 +1,16 @@
+package clueGame;
+
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class HumanCards extends JPanel{
+	HumanCards(){
+		this.setLayout(new GridLayout(0, 1));
+	}
+	
+	private void addCards(){
+		
+	}
+}
